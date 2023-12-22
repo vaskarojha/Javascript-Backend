daa7b1a7d5c336e2f78dd01f202e08a3b0b7235b
vaskarojha <vaskar.1125@gmail.com>
2023-12-22 14:44:11 +0100

    user register route

--- src/app.js
+++ src/app.js
@@ -12,12 +12,16 @@ app.use(cors({
 app.use(express.json({
     limit: "16kb"
 }))
-
 app.use(express.urlencoded({extended:true, limit:'16kb'}))
-
 app.use(express.static("pubmic"))
-
 app.use(cookieParser())
+
+
+import userRouter from "./routes/user.routes.js"
+
+//routes
+app.use("/api/v1/users", userRouter)
+
 
 
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,21 +1,21 @@
-// const asyncHandler = (fn) = async(req, res, next) =>{
-//     try{
-//         fn(req, res,next)
-//     }
-//     catch(err){
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: err.message
-//         })
-//     }
-// }
-
-const asyncHandler = (requestHandler)=>(req, res, next)=>{
-    (req, res, next)=>{
-        Promise.resolve(requestHandler(req, res, next)).catch((err)=>{
-            next(err)
+const asyncHandler = (fn) => async(req, res, next) =>{
+    try{
+        return fn(req, res,next)
+    }
+    catch(err){
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
         })
     }
 }
+
+// const asyncHandler = (requestHandler)=>(req, res, next)=>{
+//     return (req, res, next)=>{
+//         Promise.resolve(requestHandler(req, res, next)).catch((err)=>{
+//             next(err)
+//         })
+//     }
+// }
 
 export  {asyncHandler}
