053e0ced454178d8e9aacdde8f8af4206f73e038
vaskarojha <vaskar.1125@gmail.com>
2023-12-26 18:38:59 +0100

    mongodb aggregation pipeline used to fetch subscribers and channels

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -246,6 +246,70 @@ const updateUserCover = asyncHandler(async(req,res)=>{
     )
 })
 
+const getUserChannelProile = asyncHandler(async(req,res)=>{
+    const {username} = req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400, "username required!!")
+    }
+
+    const channel = await User.aggregate([
+       { $match:{
+            username: username?.toLowerCase()
+        }},
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size: "$subscribedTo"
+                },
+                isSubscribed:{
+                    $condition:{
+                        if:{$in: [req.user?._id, "subscribers.subscriber"]},
+                        then: true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullname:1,
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel does not exists!!")
+    }
+    return res.status(200).json(new ApiResponse(200, channel[0]), "User and channel data fetched!!")
+
+})
 
 
-export {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateUserAvatar, updateUserCover, updateAccountDetails}
+export {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateUserAvatar, updateUserCover, updateAccountDetails, getUserChannelProile}
