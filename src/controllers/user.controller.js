40f043ff5ceb3382cb809d715594432ef14662f2
vaskarojha <vaskar.1125@gmail.com>
2023-12-27 19:50:13 +0100

    Add aggregation sub pipeline to fetch watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -312,4 +312,50 @@ const getUserChannelProile = asyncHandler(async(req,res)=>{
 })
 
 
-export {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateUserAvatar, updateUserCover, updateAccountDetails, getUserChannelProile}
+const getWatchHistory = asyncHandler(async(req, res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id: new mangoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from : "videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullname:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    return res.status(200).json(new ApiResponse(200, user[0], watchHistory,  "Watch History fetched!!" ))
+})
+
+export {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateUserAvatar, updateUserCover, updateAccountDetails, getUserChannelProile, getWatchHistory}
