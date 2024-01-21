e9ce5fc6ca3fcb2e107b1d9abe5b1ce2314eb543
vaskarojha <vaskar.1125@gmail.com>
2024-01-21 20:26:51 +0100

    DoublyLinkedList -  Push

--- Data-structure-and-algorithm/DoublyLinkedList.js
+++ Data-structure-and-algorithm/DoublyLinkedList.js
@@ -13,8 +13,26 @@ class DoublyLinkedList{
         this.tail = newNode
         this.length = 1
     }
+    Push(value){
+        const newNode = new Node(value)
+        if(!this.head){
+            this.head =newNode
+            this.tail = newNode
+        }
+        else{
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+        this.length ++
+        return this
+    }
 }
 
+
+
 let myDoublyLinkList = new DoublyLinkedList(5)
+console.log(myDoublyLinkList)
+myDoublyLinkList.Push(7)
 console.log(myDoublyLinkList)
 
