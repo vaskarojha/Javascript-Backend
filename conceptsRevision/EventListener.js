e6ad07239bb1fe7ae070eeb3a3e2c7c6270f807e
vaskarojha <vaskar.1125@gmail.com>
2024-02-16 08:29:06 +0100

    event capturing and stopPropagation

--- conceptsRevision/EventListener.js
+++ conceptsRevision/EventListener.js
@@ -1,13 +1,45 @@
 const grandParent = document.querySelector(".grandParent")
 const parent = document.querySelector(".parent")
-const child = document.querySelector("child")
+const child = document.querySelector(".child")
+
+
+// Event bubbling: where the inner child element reacts and gradually goes to prarents until the window document.
+// child.addEventListener("click", (e)=>{
+//     console.log("child div clicked")
+// })
+
+// parent.addEventListener("click", (e)=>{
+//     console.log("parent div clicked")
+// })
+// grandParent.addEventListener("click", (e)=>{
+//     console.log("grandParent div clicked")
+// })
+
+// Event capturing: When capture paramater is made true, the event belonge that activates and whien the inner element is clicked
+// parent.addEventListener("click", (e)=>{
+//     console.log("parent div clicked")
+// }, {
+//     capture:true
+// })
+
+// child.addEventListener("click", (e)=>{
+//     console.log("child div clicked")
+// })
+// grandParent.addEventListener("click", (e)=>{
+//     console.log("grandParent div clicked")
+// })
+
+// stopPropagation()
+
+parent.addEventListener("click", (e)=>{
+    e.stopPropagation()
+    console.log("parent div clicked")
+}, {
+    capture:true
+})
 
 child.addEventListener("click", (e)=>{
     console.log("child div clicked")
-})
-
-parent.addEventListener("click", (e)=>{
-    console.log("parent div clicked")
 })
 grandParent.addEventListener("click", (e)=>{
     console.log("grandParent div clicked")
