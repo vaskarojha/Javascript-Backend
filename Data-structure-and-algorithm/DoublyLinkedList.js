b030928c4ced4e89d5fbf4261d5313eb7860547c
vaskarojha <vaskar.1125@gmail.com>
2024-02-22 04:58:45 +0100

    Doubly LL : shift

--- Data-structure-and-algorithm/DoublyLinkedList.js
+++ Data-structure-and-algorithm/DoublyLinkedList.js
@@ -56,10 +56,23 @@ class DoublyLinkedList{
         this.length++
         return this
     }
+    shift(){
+        if(this.length===0){
+            return undefined
+        }
+        let temp = this.head
+        if(this.length ===1){
+            this.head = null
+            this.tail = null
+        } else{
+            this.head = this.head.next
+            this.head.prev = null
+            temp.next = null
+        }
+        this.length --
+        return temp
+    }
 }
-
-
-
 
 
 let myDoublyLinkList = new DoublyLinkedList(5)
