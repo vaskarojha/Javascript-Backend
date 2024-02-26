4f1bb6d46ebea057e5b0064969f048fde5f15600
vaskarojha <vaskar.1125@gmail.com>
2024-02-26 23:28:20 +0100

    Doubly LL:Remove

--- Data-structure-and-algorithm/DoublyLinkedList.js
+++ Data-structure-and-algorithm/DoublyLinkedList.js
@@ -118,7 +118,21 @@ class DoublyLinkedList{
         return true
     }
 
-    
+    remove(index){
+        if(index ===0) return this.shift()
+        if(index=== this.length -1) return this.Pop()
+        if(index<0 || index>= this.length) return undefined
+        
+        const temp = this.get(index)
+
+        temp.prev.next = temp.next
+        temp.next.prev = temp.prev
+        temp.next = null
+        temp.prev = null
+
+        this.length --
+        return temp
+    }
 }
 
 
