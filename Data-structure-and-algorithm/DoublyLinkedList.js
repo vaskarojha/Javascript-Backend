12f207ed34aa149cdac002b8c13aacc7af778487
vaskarojha <vaskar.1125@gmail.com>
2024-03-05 20:31:36 +0545

    Binary search : Insert

--- Data-structure-and-algorithm/DoublyLinkedList.js
+++ Data-structure-and-algorithm/DoublyLinkedList.js
@@ -253,5 +253,36 @@ class BST{
     }
 }
 
+
+insert(value){
+    const newNode = new BstNode(value)
+    if(this.root === null){
+        this.root = newNode
+        return this
+    }
+    let temp = this.root
+    while(true){
+        if(newNode.value === temp.value) return undefined
+        if(newNode.value < temp.value){
+            if(temp.left === null){
+                temp.left = newNode
+                return this
+            }
+            temp = temp.left
+        }else{
+            if(temp.right === null){
+                temp.right = newNode
+                return this
+            }
+            temp = temp.right
+        }
+    }
+}
+
+
+
 let myTree = new BST()
 myTree
+
+myTree.insert(50)
+myTree.insert(65)
