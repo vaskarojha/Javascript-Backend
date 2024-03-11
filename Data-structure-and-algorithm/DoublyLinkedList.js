1bfd163dc0794a0b98888490d91ab824f512d989
vaskarojha <vaskar.1125@gmail.com>
2024-03-11 22:39:00 +0545

    HashTable : keys

--- Data-structure-and-algorithm/DoublyLinkedList.js
+++ Data-structure-and-algorithm/DoublyLinkedList.js
@@ -340,6 +340,18 @@ class HashTable{
         }
         return undefined
     }
+
+    keys(){
+        let allKeys = []
+        for(let i=0; i<this.dataMap.length; i++){
+            if(this.dataMap[i]){
+                for(let j = 0; j<this.dataMap[i], j++){
+                    allKeys.push(this.dataMap[i][j][0])
+                }
+            }
+        }
+        return allKeys
+    }
 }
 
 
